ebd54b370e47d1ecb6868fd62ae4b0ff586bf851
algan-calhub <algan@posteo.de>
2025-08-04 09:03:05 +0200

    Register cache storage provider

--- script.php
+++ script.php
@@ -3,13 +3,19 @@
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\LanguageHelper;
+use Joomla\CMS\Service\Provider\Cache;
 
 class com_contentintegratorInstallerScript
 {
     public function postflight($type, $parent): void
     {
         $container = Factory::getContainer();
-        $db        = $container->get('DatabaseDriver');
+
+        if (! $container->has('cache.storage')) {
+            $container->registerServiceProvider(new Cache());
+        }
+
+        $db = $container->get('DatabaseDriver');
 
         $db->setQuery("DELETE FROM #__extensions WHERE element = 'com_contentintegrator' AND type = 'component'");
         $db->execute();
