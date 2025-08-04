60629ae470342d65b759697dfcceeb78fbdcd3d9
algan-calhub <algan@posteo.de>
2025-08-04 18:24:44 +0200

    Remove cache experiment and add default dashboard

---
+++ administrator/components/com_contentintegrator/src/View/Dashboard/HtmlView.php
@@ -0,0 +1,10 @@
+<?php
+namespace Joomla\Component\Contentintegrator\Administrator\View\Dashboard;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+class HtmlView extends BaseHtmlView
+{
+}
---
+++ administrator/components/com_contentintegrator/tmpl/dashboard/default.php
@@ -0,0 +1,4 @@
+<?php defined('_JEXEC') or die; ?>
+
+<h1 class="mb-4">Content Integrator – Dashboard</h1>
+<p>Willkommen! Wählen Sie links im Menü einen Import-Wizard.</p>
